8ef571925c2af665fd5c312b32011edd3db433bb
sams-ddev <sams.alxstudent@gmail.com>
2023-07-24 18:10:52 +0000

    added the protypes to the main.h file

--- main.h
+++ main.h
@@ -7,21 +7,10 @@
 #include <stdlib.h>
 
 
-/**
- * struct print - struct for printer functions
- * @type_arg: identifier
- * @f: pointer to a printer functions
- *
- * Description: struct that stores pointers to a
- * printer functions.
- */
-
-typedef struct print
-{
-	char *type_arg;
-	int (*f)(va_list, char *, unsigned int);
-} print_t;
-
+int _printf(const char *format, ...);
+int get_print_func(const char *format, int i);
+void handl_buf(char *buffer, char c, unsigned int ibuf);
+void print_buf(char *buffer, unsigned int ibuf);
 
 
 #endif
