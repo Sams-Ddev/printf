440d5cc01a8f366d19e81f5fe1d870b0ae37bfc0
sams-ddev <sams.alxstudent@gmail.com>
2023-07-25 15:34:16 +0000

    updated code on _printf.c

--- _printf.c
+++ _printf.c
@@ -28,3 +28,26 @@ int _printf(const char *format, ...)
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+
+			i++;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+
+	return (printed_chars);
+}
