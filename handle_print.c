203e2a11085af3c3a99bcc5859df473e5b8307f1
James Limbe <limbejay254@gmail.com>
2023-07-25 08:54:21 -0700

    Handle print functions introduced

---
+++ handle_print.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * handle_print - Prints an argument based on its type
+ *
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: Index.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags.
+ * @width: Get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ *
+ * Return: Number of characters printed (positive value),
+ *         -1 if an unsupported format specifier is encountered,
+ *         or 0 for special cases.
+ */
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	int i, unknown_len = 0, printed_chars = -1;
+	//The functions and formats
+	//Edit wherrevber possible
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
