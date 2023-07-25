888706e435e6bf1273608760b70175439c7ea64b
James Limbe <limbejay254@gmail.com>
2023-07-25 09:08:27 -0700

    second set of format functions

---
+++ formatFunctionsII.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_pointer - Prints the value of a pointer variable
+ *
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ *
+ * Return: Number of characters printed
+ */
+
+int print_pointer(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	char extra_c = 0, padd = ' ';
+	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
+	UNUSED(width);
+	UNUSED(size);
+	if (addrs == NULL)
+		return (write(1, "(nil)", 5));
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
+
+	num_addrs = (unsigned long)addrs;
+
+	while (num_addrs > 0)
+	{
+		buffer[ind--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		length++;
+	}
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (flags & F_PLUS)
+		extra_c = '+', length++;
+	else if (flags & F_SPACE)
+		extra_c = ' ', length++;
+	ind++;
+
+	return (write_pointer(buffer, ind, length, width,
