193fef80c20710bed7fb66e59496705c400c7a06
James Limbe <limbejay254@gmail.com>
2023-07-25 08:46:19 -0700

    Added the print_buffer function and removed old one. Good tidings

---
+++ print_buffer.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exists
+ *
+ * @buffer: Array of characters representing the buffer
+ * @buff_ind: Pointer to the index at which to add the next character,
+ * represents the buffer length
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
