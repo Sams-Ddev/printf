21b4b8f2571b5edc2e80ca1d9d4d648d7dac30cc
James Limbe <limbejay254@gmail.com>
2023-07-25 08:39:01 -0700

    Added first functions of main_h, last function needs attention

--- main.h
+++ main.h
@@ -1,36 +1,50 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-
-#include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdarg.h>
+
+/* Buffer size for handling the output */
+#define BUFF_SIZE 1024
+
+/* Flags for formatting options */
+#define F_MINUS (1 << 0) /* '-' flag */
+#define F_PLUS  (1 << 1) /* '+' flag */
+#define F_ZERO  (1 << 2) /* '0' flag */
+#define F_HASH  (1 << 3) /* '#' flag */
+#define F_SPACE (1 << 4) /* ' ' flag */
+
+/* Size specifiers */
+#define S_LONG  (1 << 0) /* 'l' size specifier */
+#define S_SHORT (1 << 1) /* 'h' size specifier */
+
+/* Macro for marking unused variables to suppress compiler warnings */
+#define UNUSED(x) (void)(x)
 
 /**
- * struct print - struct for printer functions
- * @type_arg: identifier
- * @f: pointer to a printer functions
- *
-* Description: struct that stores pointers to a
- * printer functions.
+ * struct fmt_t - Struct to map format specifiers to corresponding functions
+ * @fmt: Format specifier character
+ * @fn: Pointer to the corresponding printing function
  */
-
-typedef struct print
+typedef struct fmt_t
 {
-	char *type_arg;
-	int (*f)(va_list, char *, unsigned int);
-} print_t;
+	char fmt;
+	int (*fn)(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+} fmt_t;
 
-
+/* Function prototypes */
 int _printf(const char *format, ...);
-int get_print_func(const char *format, int i);
-void handl_buf(char *buffer, char c, unsigned int ibuf);
-void print_buf(char *buffer, unsigned int ibuf);
-int print_chr(va_list arguments, char *buffer, unsigned int buffer_index);
-int ev_print_func(const char *s, int index);
-int print_str(va_list arguments, char *buf, unsigned int ibuf);
-
-
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int is_printable(char c);
+int append_hexa_code(char ascii_code, char buffer[], int i);
+int is_digit(char c);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],)
 
 
 #endif
